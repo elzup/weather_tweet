99d954b9a98ca8e091fe9a73e810cae71b6ce451
elzzup <guild0105@gmail.com>
2015-05-27 08:42:21 +0900

    refactor module

--- cms/views.py
+++ cms/views.py
@@ -7,50 +7,52 @@ from geoposition import Geoposition
 from cms.models import RainMeshMap, RainMesh, MeshPosition
 import csv
 
+
+def regist_mesh(rain_map, lat, lng, rate):
+    # 明らかにおかしなデータが混じっていたので
+    if lat < 35.0 or lat >= 38 or lng < 137.0 or lng >= 139:
+        return
+    if rate == 0:
+        return
+    try:
+        pos = MeshPosition.objects.get(position=Geoposition(lat, lng))
+    except ObjectDoesNotExist:
+        pos = MeshPosition(position=Geoposition(lat, lng))
+        pos.save()
+    try:
+        mesh = RainMesh.objects.get(map_id=rain_map.id, mesh_position_id=pos.id)
+    except ObjectDoesNotExist:
+        mesh = RainMesh(map_id=rain_map.id, mesh_position_id=pos.id, rain_rate_x10=int(rate * 10))
+        mesh.save()
+
+    print(lat, lng, rate)
+    print(mesh.id)
+
+
 def import_data(request):
     """降水量データcsv のインポートみ込み"""
-    day = 20
-    hour = 23
-    minute = 21
-    file_name = "./data/test_2015-05-%02d_%02d-%02d.csv" % (day, hour, minute)
+    # 開始日
+    # itime = timezone.datetime(2015, 5, 20, 23, 21)
+    itime = timezone.datetime(2015, 5, 20, 23, 31)
+    end_time = timezone.datetime(2015, 5, 21, 6, 49)
 
-    with open(file_name) as f:
-        reader = csv.reader(f)
-        header = next(reader)
+    file_name = "./data/test_2015-05-%02d_%02d-%02d.csv" % (itime.date().day, itime.hour, itime.minute)
 
-        time_map = timezone.datetime(2015, 5, day, hour, minute)
-        try:
-            rain_map = RainMeshMap.objects.get(time_map=time_map)
-        except ObjectDoesNotExist:
-            rain_map = RainMeshMap(time_map=time_map)
-            rain_map.save()
+    min1 = timezone.timedelta(minutes=1)
 
-        print(rain_map)
-        print(rain_map.id)
-
-        c = 0
-        for row in reader:
-            lat, lng, rate = map(float, row)
-            if rate == 0:
-                continue
+    while(itime <= end_time):
+        with open(file_name) as f:
+            reader = csv.reader(f)
+            header = next(reader)
+            time_map = itime
             try:
-                pos = MeshPosition.objects.get(position=Geoposition(lat, lng))
-                print('skip')
+                rain_map = RainMeshMap.objects.get(time_map=time_map)
             except ObjectDoesNotExist:
-                pos = MeshPosition(position=Geoposition(lat, lng))
-                pos.save()
-            try:
-                mesh = RainMesh.objects.get(map_id=rain_map.id, mesh_position_id=pos.id)
-                print('skip')
-            except ObjectDoesNotExist:
-                mesh = RainMesh(map_id=rain_map.id, mesh_position_id=pos.id, rain_rate_x10=int(rate * 10))
-                mesh.save()
-
-            c += 1
-            print(lat, lng, rate)
-            print(mesh.id)
-            print(c)
-            if c == 20:
-                break
+                rain_map = RainMeshMap(time_map=time_map)
+                rain_map.save()
+            for row in reader:
+                lat, lng, rate = map(float, row)
+                regist_mesh(rain_map, lat, lng, rate)
+        itime += min1
 
     return HttpResponse(u'インポート')
